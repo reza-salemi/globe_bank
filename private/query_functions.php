b600470c151896705e91d1e4cecc4b4747b7daeb
reza-salemi <en.salemi@outlook.com>
2021-05-19 08:57:16 +0430

    wrtie delete function for subject

--- private/query_functions.php
+++ private/query_functions.php
@@ -72,6 +72,25 @@ function update_subject($subject)
 
 }
 
+function delete_subject($id)
+{
+    global $db;
+
+    $sql = "DELETE FROM subjects ";
+    $sql .= "WHERE id='" . $id . "' ";
+    $sql .= "LIMIT 1";
+    $result = mysqli_query($db, $sql);
+
+    if ($result){
+        return true;
+    }
+    else{
+        echo mysqli_error($db);
+        db_disconnect($db);
+        exit();
+    }
+}
+
 function find_all_pages()
 {
     global $db;
--- public/staff/subjects/index.php
+++ public/staff/subjects/index.php
@@ -34,7 +34,7 @@ $subject_set = find_all_subjects();
                     <td><?php echo h($subject['menu_name']); ?></td>
                     <td><a class="action" href="<?php echo url_for('staff/subjects/show.php?id='. h(u($subject['id']))); ?>">View</a></td>
                     <td><a class="action" href="<?php echo url_for('staff/subjects/edit.php?id='. h(u($subject['id']))); ?>">Edit</a></td>
-                    <td><a class="action" href="">Delete</a></td>
+                    <td><a class="action" href="<?php echo url_for('staff/subjects/delete.php?id=' . h(u($subject['id'])));?>">Delete</a></td>
                 </tr>
             <?php } ?>
         </table>
