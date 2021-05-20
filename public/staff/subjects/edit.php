65b7b617c0104c62970c77f548a9468eaf5936f5
reza-salemi <en.salemi@outlook.com>
2021-05-20 08:37:06 +0430

    write display error function

--- public/staff/subjects/edit.php
+++ public/staff/subjects/edit.php
@@ -17,14 +17,22 @@ if(is_post_request())
     $subject['visible'] = $_POST['visible'] ?? '';
 
     $result = update_subject($subject);
-    redirect_to(url_for('/staff/subjects/show.php?id=' . $id));
+    if ($result === true){
+        redirect_to(url_for('/staff/subjects/show.php?id=' . $id));
+    }
+    else{
+        $errors = $result;
+    }
+
 }
 else{
     $subject = find_subject_by_id($id);
-    $subject_set = find_all_subjects();
-    $subject_count = mysqli_num_rows($subject_set);
-    mysqli_free_result($subject_set);
+
 }
+
+$subject_set = find_all_subjects();
+$subject_count = mysqli_num_rows($subject_set);
+mysqli_free_result($subject_set);
 
 ?>
 
@@ -38,7 +46,8 @@ else{
     <div class="subject edit">
         <h1>Edit Subject</h1>
 
-        <form action="" method="post">
+        <?php echo display_errors($errors); ?>
+        <form action="<?php echo url_for('/staff/subjects/edit.php?id='.h(u($id))); ?>" method="post">
             <dl>
                 <dt>Menu Name</dt>
                 <dd><input type="text" name="menu_name" value="<?php echo $subject['menu_name'] ;?>" /></dd>
